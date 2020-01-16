e9bf3943d5f079cd9ac1e8a4198f4c6d60baeb7a
Hetal Parmar <hetal_parmar@persistent.co.in>
2020-01-16 11:32:45 +0530

    first commit

---
+++ src/HomePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <header className="header">
+        <div className="inner div"> </div>
+        </header> 
+    );
+    }
+}
+ 
+export default HomePage ;
